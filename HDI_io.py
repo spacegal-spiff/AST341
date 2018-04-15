e1fac72b6ec6d1ce6575703cdebabf81fdb170fc
Michael <petersen.michael.s@gmail.com>
2018-04-14 20:30:37 -0400

    Add wcs capability to HDI_io

--- HDI_io.py
+++ HDI_io.py
@@ -1,14 +1,19 @@
 # ability to read/write fits files
 from astropy.io import fits
 
+# standard numpy import
 import numpy as np
+
+# World Coordinate System ability
+from astropy.wcs import WCS
+
 
 
 def read_image(infile,mosaic=True):
     '''
     read_image
     ----------
-    routine to read in an image and either return
+    routine to read in a 4 amp HDI image and either return
     
     mosaic==True
         single mosaicked, non-overscan frame
@@ -140,5 +145,99 @@ def find_files(directory,img_type,filternum=-1):
 
 
     return out_files
+
+
+
+
+def verify_hdi_filter(infile,des_filter,verbose=False):
+    '''
+    verify_filter
+    ---------------
+    check that the filter we think is being used IS being used
+    
+    inputs
+    ---------
+    infile      : (string) filename of image to check
+    des_filter  : (string) common filter name (see filter_dict below)
+    verbose     : (boolean, default=False) if True, print reason for failure
+    
+    returns
+    ---------
+    0,1,2       : (int code) 0=filter matches; 1=filter does not match; 2=one wheel slot not empty
+    
+    '''
+    
+    # specify the filter dictionary; hardcoded because it will not change
+    filter_dict = {'V':'102',\
+               'R':'103',\
+               'I':'104',\
+               'Ha':'200',\
+               'Haoff':'204',\
+              'empty':['105','106','107','205','206','207']}
+    
+    # get the header
+    phdr = fits.getheader(infile,0)
+    
+    # check that a wheel slot is empty
+    if ((phdr['FILTER1'] not in filter_dict['empty']) & (phdr['FILTER2'] not in filter_dict['empty']) ):
+        
+        if verbose:
+            print('verify_filter: failed with multiple filters.')
+            
+        return 2
+    
+    # check that filter matches the desired wheel position
+    elif  (( phdr['FILTER1'] == filter_dict[des_filter]) | ( phdr['FILTER2'] == filter_dict[des_filter]) ):
+        return 0
+    
+    # default to failure mode
+    else:
+        if verbose:
+            print('verify_filter: failed with non-matching filter.')
+            
+        return 1
+    
+    
+
+    
+
+def construct_astrometry(hdr_wcs):
+    '''
+    construct_astrometry
+    
+    make the pixel to RA/Dec conversion (and back) from the header of an astrometry.net return
+    
+    inputs
+    ------------------------------
+    hdr_wcs  :  header with astrometry information, typically from astrometry.net
+    
+    returns
+    ------------------------------
+    w        :  the WCS instance
+    
+    '''
+    
+    # initialize the World Coordinate System
+    w = WCS(naxis=2)
+    
+    # specify the pixel to RA/Dec conversion
+    w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+    w.wcs.cd = np.array([[hdr_wcs['CD1_1'],hdr_wcs['CD1_2']],[hdr_wcs['CD2_1'],hdr_wcs['CD2_2']]])
+    w.wcs.crpix = [hdr_wcs['CRPIX1'], hdr_wcs['CRPIX2']]
+    w.wcs.crval = [hdr_wcs['CRVAL1'],hdr_wcs['CRVAL2']]
+    w.wcs.cunit = [hdr_wcs['CUNIT1'],hdr_wcs['CUNIT2']]
+    w.wcs.latpole = hdr_wcs['LATPOLE']
+    #w.wcs.lonpole = hdr_wcs['LONPOLE']
+    w.wcs.theta0 = hdr_wcs['LONPOLE']
+    w.wcs.equinox = hdr_wcs['EQUINOX']
+
+    # calculate the RA/Dec to pixel conversion
+    w.wcs.fix()
+    w.wcs.cdfix()
+    w.wcs.set()
+    
+    # return the instance
+    return w
+
 
 
